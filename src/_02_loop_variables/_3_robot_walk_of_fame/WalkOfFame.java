042bc72f24ceb5004855b66b1196c4a49901b2c2
root <root@e694f0d8f6ba>
2021-08-25 01:27:48 +0000

    walkofffame and bullseye

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,10 +9,31 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left.
 		rob.setX(50);
+		
 		// You also need to show the robot to see the result of this line.
                                                                          
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+		int i = 0;
+		int ii = 0;
+		rob.setAngle(144-90);
+		rob.penDown();
+		rob.setPenWidth(5);
+		rob.setSpeed(40);
+		while(ii < 10) {
+			while(i < 5) {
+				rob.move(30);
+				rob.turn(72);
+				rob.move(30);
+				rob.turn(-144);
+				i++;
+			}
+	    rob.penUp();
+		rob.setX(rob.getX() + 80);
+		rob.penDown();
+		i -= 5;
+		ii++;
+		}
+		
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
--- src/_03_modulo/_2_bullseye/bullseye/bullseye.pde
+++ src/_03_modulo/_2_bullseye/bullseye/bullseye.pde
@@ -1,13 +1,27 @@
 
 void setup() {
  
-  // set the size of your sketch
+  size(200,200);
   
 }
 
 void draw() {
+  color c1 = color(0,0,0);
+  color c2 = color(255,0,0);
+  int size = 95;
+  fill(c2);
   
-  //Starting with the largest ellipse, use a for loop to draw a bullseye with ellipses.
+  for(int i = 0; i < 9; i++) {
+    if(i % 2 == 0) {
+      stroke(c1);
+      strokeWeight(10);
+    }
+    else {
+      stroke(c2);
+    }
+  ellipse(100,100,size-10,size-10); 
+  size-=10;
+  }
   
   //Use an if statement and modulo to alternate the color of your rings.
   
